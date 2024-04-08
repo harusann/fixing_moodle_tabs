b856eb7e2d9a25959caa23b3745089396e5a4799
harusann <gp22a074@oecu.jp>
2024-04-08 13:50:38 +0900

    前期後期が1月ずれるバグを解消

--- option/options.js
+++ option/options.js
@@ -3,7 +3,7 @@ const $select_semester = document.getElementById('semester');
 $select_semester.onchange = (event) => {
     let num = Number(event.currentTarget.value);
     let today = new Date();
-    if (today.getMonth() >= 9 || today.getMonth() <= 3) num += 4;
+    if (today.getMonth() >= 8 || today.getMonth() <= 2) num += 4;
     let options = {
         num:num
     }
@@ -16,7 +16,8 @@ chrome.storage.sync.get(null, (options) => {
         $select_semester.options[0].selected = true;
         let num = 1;
         let today = new Date();
-        if (today.getMonth() >= 9 || today.getMonth() <= 3) num += 4;
+        // 月情報は0から11まで
+        if (today.getMonth() >= 8 || today.getMonth() <= 2) num += 4;
         let options = {
             num:num
         }
@@ -24,8 +25,6 @@ chrome.storage.sync.get(null, (options) => {
         return;
     } else {
         let id = options.num;
-        console.log(id)
-        console.log((id - 1) % 4);
         $select_semester.options[(id - 1) % 4].selected = true;
     }
 });
