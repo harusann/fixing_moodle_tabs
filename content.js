f6140a7d0890b46e4daf5e2652d394aa18f1f2b7
harusann <gp22a074@oecu.jp>
2024-10-28 16:06:57 +0900

    前期と後期の切り替わりがないバグを除去

--- content.js
+++ content.js
@@ -2,8 +2,10 @@ window.onload = () => {
     chrome.storage.sync.get(null, (options) => {
         let num = options.num;
         if (num === undefined) return;
+        let today = new Date();
+        // 月情報は0から11まで
+        if (today.getMonth() >= 8 || today.getMonth() <= 2) num += 4;
         let id = 'btn-g' + num + '-f';
-        console.log(id);
         document.getElementById(id).click();
     });
 };
--- option/options.js
+++ option/options.js
@@ -2,8 +2,6 @@ const $select_semester = document.getElementById('semester');
 
 $select_semester.onchange = (event) => {
     let num = Number(event.currentTarget.value);
-    let today = new Date();
-    if (today.getMonth() >= 8 || today.getMonth() <= 2) num += 4;
     let options = {
         num:num
     }
@@ -12,14 +10,9 @@ $select_semester.onchange = (event) => {
 
 chrome.storage.sync.get(null, (options) => {
     if (options.num === undefined) {
-        console.log('undifined ! ')
         $select_semester.options[0].selected = true;
-        let num = 1;
-        let today = new Date();
-        // 月情報は0から11まで
-        if (today.getMonth() >= 8 || today.getMonth() <= 2) num += 4;
         let options = {
-            num:num
+            num:1
         }
         chrome.storage.sync.set(options);
         return;
